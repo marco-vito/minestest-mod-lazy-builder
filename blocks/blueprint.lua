f453725d4ffbca8e1f0fcb2c6500fd7b77ffa2d8
stealthio <20127926+stealthio@users.noreply.github.com>
2020-10-23 00:40:41 +0200

    Implemented schematic analysis and block replacement

--- blocks/blueprint.lua
+++ blocks/blueprint.lua
@@ -17,8 +17,24 @@ minetest.register_node(minetest.get_current_modname()..":blueprint_selector",{
 	tiles = {"Builder.png"},
 	walkable = true,
 	after_place_node = function(pos, placer, itemstack, pointed_thing)
-		local path = minetest.get_modpath("lazybuilder").."/schematics/House_Simple_1.mts"
-		minetest.place_schematic(pos, path, "0",{["wool:red"] = "lazybuilder:blueprint", ["wool:green"] = "lazybuilder:blueprint"} , false)
+		local path = minetest.get_modpath("lazybuilder").."/schematics/House_Simple.mts"
+		local schematic  = minetest.read_schematic(path, {})
+		local blocks = {}
+		for k,v in pairs(schematic.data) do
+			if not (v.name == 0 or v.name == "air" or v.name == 254) then
+				if blocks[v.name] then
+					blocks[v.name] = blocks[v.name] + 1
+				else 
+					blocks[v.name] = 1
+				end
+				v.name = "lazybuilder:blueprint"
+			end
+		end
+		for k,v in pairs(blocks) do
+			minetest.chat_send_all(tostring("K: " .. tostring(k).. " V: " .. tostring(v)))
+		end
+
+		minetest.place_schematic(pos, schematic, "0",{} , false)
 	end,
 	on_rightclick = function(pos, node, player, itemstack, pointed_thing)
 		-- TODO
