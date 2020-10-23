a380c3fec3669a2fa755cbc8d3a1f569afd7eace
stealthio <20127926+stealthio@users.noreply.github.com>
2020-10-23 13:42:51 +0200

    Refactoring

--- blocks/blueprint.lua
+++ blocks/blueprint.lua
@@ -11,6 +11,7 @@ minetest.register_node(minetest.get_current_modname()..":blueprint",{
 	end
 })
 
+-- builds the UI dynamically depending on the amount of different blocks in the schematic
 local function get_selector_formspec(pos)
 	local formspec = {
 		"formspec_version[3]",
@@ -29,6 +30,33 @@ local function get_selector_formspec(pos)
 	return table.concat(formspec, "")
 end
 
+-- returns a table containing the blocks in a schematic
+-- key: name of the block e.g. "default:dirt"
+-- value: count of the block
+-- the blocks "air" and other blocks that are used for background work are ignored
+local function get_blocks_from_schematic(schematic)
+	local blocks = {}
+	for k,v in pairs(schematic.data) do
+		if not (v.name == 0 or v.name == "air" or v.name == 254) then
+			if blocks[v.name] then
+				blocks[v.name] = blocks[v.name] + 1
+			else 
+				blocks[v.name] = 1
+			end
+		end
+	end
+	return blocks
+end
+
+-- replaces all the blocks in a schematic with the given 'block' parameter
+local function replace_blocks_in_schematic(schematic, block)
+	for k,v in pairs(schematic.data) do
+		if not (v.name == 0 or v.name == "air" or v.name == 254) then
+			v.name = block
+		end
+	end
+end
+
 -- Used to place down blueprints
 minetest.register_node(minetest.get_current_modname()..":blueprint_selector",{
 	description = "Blueprint selector",
@@ -36,39 +64,26 @@ minetest.register_node(minetest.get_current_modname()..":blueprint_selector",{
 	walkable = true,
 
 	on_construct = function(pos)
-		-- Placing schematic and saving it
 		local path = minetest.get_modpath("lazybuilder").."/schematics/House_Simple.mts"
 		local schematic  = minetest.read_schematic(path, {})
-		local blocks = {}
-		for k,v in pairs(schematic.data) do
-			if not (v.name == 0 or v.name == "air" or v.name == 254) then
-				if blocks[v.name] then
-					blocks[v.name] = blocks[v.name] + 1
-				else 
-					blocks[v.name] = 1
-				end
-				v.name = "lazybuilder:blueprint"
-			end
-		end
-
+		local blocks = get_blocks_from_schematic(schematic)
+		replace_blocks_in_schematic(schematic, "lazybuilder:blueprint")
 		minetest.place_schematic(pos, schematic, "0",{} , false)
 		
-		-- preparing the inventory of the placer --
-		
+
+		-- to allow access in the formspec we need to save the blocks information in the metadata
 		local meta = minetest.get_meta(pos)
-		meta:set_string("blocks", minetest.serialize(blocks))
+		meta:set_string("blocks", minetest.serialize(blocks)) 
 	
-		local meta = minetest.get_meta(pos)
+		-- preparing the inventory of the placer --
 		local inv = meta:get_inventory()
-		for k,v in pairs(blocks) do
+		for k,v in pairs(blocks) do -- Create an inventory for each different item in blocks
 			inv:set_size(tostring(k), 1)
 		end
 	
 		meta:set_string("formspec", get_selector_formspec(pos))
 	end,
 	on_receive_fields = function(pos, formname, fields, player)
-		if formname == "lazybuilder:selector" then
-			print("TEST")
-		end
+		
 	end
 })
